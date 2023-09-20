ad82c429a8374e00fa1b9de915b1b8b057ff4ad9
izharus <ruslan.izhakovskij@gmail.com>
2023-09-20 06:05:44 +0300

    Chore(Example.py): Update example usage

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
-from src.logger_manager import log, DefaultConfig
-
+from src.logger_manager import DefaultConfig
+from src.logger_manager import log
+log = log()
 
 def main():
     """
@@ -7,21 +8,21 @@ def main():
     """
     DefaultConfig(log_dir = 'data/log')
     proc_id = "12345"
-    log().info("some info")
-    log().error("some error")
-    log().debug("some debug")
-    log().critical("some critical")
-    with log().insert_proc_id(proc_id):
-        log().info("here should be proc id")
-        with log().insert_func_name():
-            log().info("here also should be proc id and a func name")
-            with log().insert_func_name():
-                log().info("here also should be proc id and a func name")
-    with log().insert_func_name():
-        log().info("here should be only a func name")
-        with log().insert_proc_id(proc_id):
-            log().info("here also should be proc id and a func name")
-    log().info("some info")
+    log.info("some info")
+    log.error("some error")
+    log.debug("some debug")
+    log.critical("some critical")
+    with log.insert_proc_id(proc_id):
+        log.info("here should be proc id")
+        with log.insert_func_name():
+            log.info("here also should be proc id and a func name")
+            with log.insert_func_name():
+                log.info("here also should be proc id and a func name")
+    with log.insert_func_name():
+        log.info("here should be only a func name")
+        with log.insert_proc_id(proc_id):
+            log.info("here also should be proc id and a func name")
+    log.info("some info")
 if __name__ == '__main__':
     main()
 
